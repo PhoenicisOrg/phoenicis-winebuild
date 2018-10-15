1fe7c47231b984620e8b0c0a9df3f5f78120168b
Quentin PARIS <git@quentin.paris>
2018-10-15 21:35:38 +0200

    Linux AMD64

--- example_linux_amd64.py
+++ example_linux_amd64.py
@@ -4,16 +4,15 @@ from core.Container import Container
 from core.Environment import Environment
 from builders.WineBuilder import WineBuilder
 
-environment = Environment("wine", "linux", "x86")
+environment = Environment("wine", "linux", "amd64")
 environment.build()
 
-container = Container(environment).with_log_file("test_log.log")
 
-
+container = Container(environment)
 container.start()
 
 builder = WineBuilder(container)
-builder.build("builders/builder_linux_x86_wine", "wine-3.0.3.tar.gz")
+builder.build("builders/builder_linux_amd64_wine", "wine-3.0.3-amd64.tar.gz")
 builder.archive()
 
 container.clean()
