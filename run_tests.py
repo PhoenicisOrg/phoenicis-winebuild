dfa920e8ff74276a773d76a751d7f2f4d4f342da
Quentin PARIS <git@quentin.paris>
2018-10-21 23:01:05 +0200

    PhoenicisWinePackageCreationTask

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,6 @@
 import unittest
 from tests.core.DockerStepReaderTest import DockerStepReaderTest
+from tests.builders.BuilderStageReaderTest import BuilderStageReaderTest
 
 if __name__ == '__main__':
     unittest.main()
