0130ebf86dabc24816c1de422c96c39505553106
Quentin PARIS <git@quentin.paris>
2018-11-19 21:17:35 +0100

    Wine staging support

--- builders/WineBuilder.py
+++ builders/WineBuilder.py
@@ -3,18 +3,25 @@ import tempfile
 from core.Container import Container
 from core.Process import run
 
+
 class WineBuilder:
-    def __init__(self, container: Container, patches = []):
+    def __init__(self, container: Container, patches=[]):
         self.container = container
         self.patches = patches
 
-    def prepare(self, version):
+    def prepare(self, version, distribution="upstream"):
         self.container.run(["git", "clone", "https://github.com/wine-mirror/wine", "/root/wine-git"])
-        self.container.run(["git", "checkout", "-f", version], workdir = "/root/wine-git")
+        self.container.run(["git", "checkout", "-f", version], workdir="/root/wine-git")
+
+        if distribution == "staging":
+            self.container.run(["git", "clone", "https://github.com/wine-staging/wine-staging", "/root/wine-staging"])
+            self.container.run(["git", "checkout", "-f", "v" + version], workdir="/root/wine-staging")
+            self.container.run(["./patches/patchinstall.sh", "DESTDIR=\"/root/wine-git\"", "--all"], workdir="/root/wine-staging")
+
         self._apply_patches()
 
-    def build(self, script, version):
-        self.prepare(version)
+    def build(self, script, version, distribution="upstream"):
+        self.prepare(version, distribution)
         self.container.run_script(script)
 
     def archive(self, local_file):
@@ -30,4 +37,4 @@ class WineBuilder:
     def _apply_patch(self, patch):
         self.container.run(["mkdir", "-p", "/root/patches"])
         self.container.put_directory("patches/" + patch, "/root/patches/" + patch)
-        self.container.run(["sh", "-c", "git apply /root/patches/"+patch+"/*.patch"], workdir = "/root/wine-git")
+        self.container.run(["sh", "-c", "git apply /root/patches/" + patch + "/*.patch"], workdir="/root/wine-git")
---
+++ examples/wine_staging_builder.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from core.Container import Container
+from core.Environment import Environment
+from builders.WineBuilder import WineBuilder
+
+environment = Environment("wine", "linux", "x86")
+environment.build()
+
+container = Container(environment)
+container.start()
+
+
+builder = WineBuilder(container)
+builder.build("builders/scripts/builder_linux_x86_wine", "wine-3.8", "staging")
+builder.archive("wine-staging.tar.gz")
