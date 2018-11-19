40923faf401f965ba5cd04e3e8a95aa33352597e
Quentin PARIS <git@quentin.paris>
2018-11-19 22:43:15 +0100

    Interactive builder: staging support

--- examples/interactive_builder.py
+++ examples/interactive_builder.py
@@ -2,19 +2,22 @@
 from wine.WineVersionFetcher import WineVersionFetcher
 from packagers.PhoenicisWinePackageCreator import PhoenicisWinePackageCreator
 
+
 def input_from_choices(choices, text):
     print("Choices: " + ", ".join(choices))
     result = input(text)
-    if(result not in choices):
+    if result not in choices:
         print("Invalid value")
         return input_from_choices(choices, text)
     return result
 
-version = input_from_choices([version["name"] for version in WineVersionFetcher().fetch_versions(majors = [1, 2, 3])], "Please select a wine version to build: ")
+
+version = input_from_choices([version["name"] for version in WineVersionFetcher().fetch_versions(majors=[1, 2, 3])],
+                             "Please select a wine version to build: ")
 
 arch = input_from_choices(["x86", "amd64"], "Choose an architecture: ")
 os = input_from_choices(["linux", "darwin"], "Choose an OS: ")
-distribution = input_from_choices(["upstream"], "Choose a distribution: ")
+distribution = input_from_choices(["upstream", "staging"], "Choose a distribution: ")
 
 builder = PhoenicisWinePackageCreator()
 
