2d0482aa20f4386e0344bc3fc17cb6ae292e9990
Quentin PARIS <git@quentin.paris>
2018-11-19 21:55:15 +0100

    Wine staging fix + error management

--- core/Container.py
+++ core/Container.py
@@ -5,6 +5,7 @@ from core.Process import run
 from core.TarUtils import make_tarfile
 from core.LineBuffer import LineBuffer
 
+
 class Container:
     @staticmethod
     def from_id(container_id):
@@ -16,7 +17,7 @@ class Container:
         self.container = None
         self.environment = environment
         self._logfile = None
-        self._output_callback = lambda line: print(line, end = '')
+        self._output_callback = lambda line: print(line, end='')
         self.with_docker_client(DockerClient.from_env())
 
     def with_output_callback(self, output_callback):
@@ -32,9 +33,10 @@ class Container:
         return self
 
     def start(self):
-        self.container = self.docker_client.client.containers.run(self.environment.full_name(), detach = True, stdin_open = True, tty = True)
+        self.container = self.docker_client.client.containers.run(self.environment.full_name(), detach=True,
+                                                                  stdin_open=True, tty=True)
 
-    def run(self, command, workdir = None):
+    def run(self, command, workdir=None):
         buffer = LineBuffer()
 
         if self._logfile is not None:
@@ -45,8 +47,10 @@ class Container:
         def do_output(chunk):
             if self._output_callback is not None:
                 buffer.append(chunk)
-                if(not buffer.empty()):
+                if not buffer.empty():
                     for line in buffer.get():
+                        if line.startswith("[THROW] "):
+                            raise Exception("Builder threw an exception: " + line)
                         self._output_callback(line)
 
             if logfile is not None:
@@ -54,33 +58,41 @@ class Container:
                 logfile.flush()
 
         try:
-            if(workdir is None):
-                execution = self.container.exec_run(command, stdout = True, stderr = True, stream = True, privileged = True)
-            else:
-                execution = self.container.exec_run(command, stdout = True, stderr = True, workdir = workdir, stream = True)
+            resp = self.docker_client.api_client.exec_create(
+                self.container.id, command, stdout=True, stderr=True, stdin=False, tty=False,
+                privileged=True, user='', environment=None,
+                workdir=workdir
+            )
+            exec_output = self.docker_client.api_client.exec_start(
+                resp['Id'], detach=False, tty=False, stream=True, socket=False
+            )
 
-            for response in execution:
-                if response is not None:
-                    for chunk in response:
-                        do_output(chunk.decode())
+            for chunk in exec_output:
+                exit_code = self.docker_client.api_client.exec_inspect(resp['Id'])['ExitCode']
+                if exit_code != 0 and exit_code is not None:
+                    raise Exception("Builder returned a non-zero code: " + str(exit_code))
+
+                do_output(chunk.decode())
+
+
         finally:
-            if(logfile is not None):
+            if logfile is not None:
                 logfile.close()
 
     def put_directory(self, local_directory, remote_directory):
         tar_file = make_tarfile(local_directory)
         self.run(["mkdir", "-p", remote_directory])
-        self.put_file(tar_file.name, remote_directory, remote_file_name = "temp.tar.gz")
-        self.run(["tar", "xvf", "temp.tar.gz"], workdir = remote_directory)
-        self.run(["rm", "temp.tar.gz"], workdir = remote_directory)
+        self.put_file(tar_file.name, remote_directory, remote_file_name="temp.tar.gz")
+        self.run(["tar", "xvf", "temp.tar.gz"], workdir=remote_directory)
+        self.run(["rm", "temp.tar.gz"], workdir=remote_directory)
 
-    def put_file(self, local_file, remote_directory, remote_file_name = None):
-        if(remote_file_name is None):
+    def put_file(self, local_file, remote_directory, remote_file_name=None):
+        if remote_file_name is None:
             remote_file_name = os.path.basename(local_file)
 
         tarstream = io.BytesIO()
         with tarfile.open(fileobj=tarstream, mode='w') as tarfile_:
-            with open(local_file, mode = 'rb') as scriptfile:
+            with open(local_file, mode='rb') as scriptfile:
                 encoded_file_contents = scriptfile.read()
                 tarinfo = tarfile.TarInfo(remote_file_name)
                 tarinfo.size = len(encoded_file_contents)
@@ -88,17 +100,17 @@ class Container:
 
         tarstream.seek(0)
         self.docker_client.api_client.put_archive(
-            container = self.container.id,
-            path = remote_directory,
-            data = tarstream
+            container=self.container.id,
+            path=remote_directory,
+            data=tarstream
         )
 
     def get_file(self, file, local_file):
-        if(local_file is None):
+        if (local_file is None):
             local_file = "archive.tar.gz"
 
         strm, stat = self.docker_client.api_client.get_archive(self.container.id, file)
-        with open(local_file, mode = 'wb') as outfile:
+        with open(local_file, mode='wb') as outfile:
             for d in strm:
                 outfile.write(d)
 
