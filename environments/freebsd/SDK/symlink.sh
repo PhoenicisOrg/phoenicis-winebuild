666151d6ecc28c8671a20f5f4dd15649d6d7f58f
ImperatorS79 <fevrier.simon@gmail.com>
2018-12-28 17:52:33 +0100

    [WIP] Add FreeBSD environment (#42)

---
+++ environments/freebsd/SDK/symlink.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/lib80211.so.1 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/lib80211.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libalias.so.7 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libalias.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libavl.so.2 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libavl.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libbe.so.1 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libbe.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libbegemot.so.4 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libbegemot.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libbsdxml.so.4 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libbsdxml.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libcam.so.7 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libcam.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/casper/libcap_dns.so.1 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libcap_dns.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/casper/libcap_grp.so.1 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libcap_grp.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/casper/libcap_pwd.so.1 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libcap_pwd.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/casper/libcap_random.so.1 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libcap_random.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/casper/libcap_sysctl.so.1 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libcap_sysctl.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/casper/libcap_syslog.so.1 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libcap_syslog.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libcasper.so.1 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libcasper.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libcrypt.so.5 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libcrypt.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libcrypto.so.111 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libcrypto.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libctf.so.2 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libctf.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libcxxrt.so.1 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libcxxrt.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libdevstat.so.7 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libdevstat.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libdtrace.so.2 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libdtrace.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libedit.so.7 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libedit.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libelf.so.2 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libelf.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libgcc_s.so.1 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libgcc_s.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libgeom.so.5 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libgeom.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libibverbs.so.1 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libibverbs.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libipsec.so.4 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libipsec.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libipt.so.0 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libipt.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libjail.so.1 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libjail.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libkiconv.so.4 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libkiconv.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libkvm.so.7 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libkvm.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libm.so.5 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libm.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libmd.so.6 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libmd.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libmlx5.so.1 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libmlx5.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libmt.so.5 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libmt.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libncurses.so.8 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libncurses.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libncursesw.so.8 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libncursesw.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libnv.so.0 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libnv.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libnvpair.so.2 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libnvpair.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libpcap.so.8 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libpcap.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libpjdlog.so.0 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libpjdlog.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/librss.so.1 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/librss.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libsbuf.so.6 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libsbuf.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libssp.so.0 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libssp.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libthr.so.3 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libthr.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libufs.so.6 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libufs.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libulog.so.0 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libulog.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libumem.so.2 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libumem.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libutil.so.9 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libutil.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libuutil.so.2 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libuutil.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libxo.so.0 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libxo.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libz.so.6 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libz.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libzfs_core.so.2 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libzfs_core.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libzfs.so.3 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libzfs.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libzpool.so.2 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libzpool.so
+
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libc.so.7 /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libc.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libthr.a /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libpthread.a
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libthr.so /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libpthread.so
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libthr_p.a /usr/cross-freebsd/x86_64-pc-freebsd12/lib/libpthread_p.a
+
+
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/lib/include /usr/cross-freebsd/x86_64-pc-freebsd12/include
+
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/include/sys/_semaphore.h /usr/cross-freebsd/x86_64-pc-freebsd12/include/_semaphore.h
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/include/sys/aio.h /usr/cross-freebsd/x86_64-pc-freebsd12/include/aio.h
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/include/sys/errno.h /usr/cross-freebsd/x86_64-pc-freebsd12/include/errno.h
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/include/sys/fcntl.h /usr/cross-freebsd/x86_64-pc-freebsd12/include/fcntl.h
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/include/sys/linker_set.h /usr/cross-freebsd/x86_64-pc-freebsd12/include/linker_set.h
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/include/sys/poll.h /usr/cross-freebsd/x86_64-pc-freebsd12/include/poll.h
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/include/sys/sched.h /usr/cross-freebsd/x86_64-pc-freebsd12/include/sched.h
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/include/sys/stdatomic.h /usr/cross-freebsd/x86_64-pc-freebsd12/include/stdatomic.h
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/include/sys/stdint.h /usr/cross-freebsd/x86_64-pc-freebsd12/include/stdint.h
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/include/sys/syslog.h /usr/cross-freebsd/x86_64-pc-freebsd12/include/syslog.h
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/include/sys/ucontext.h /usr/cross-freebsd/x86_64-pc-freebsd12/include/ucontext.h
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/include/semaphore.h /usr/cross-freebsd/x86_64-pc-freebsd12/include/sys/semaphore.h
+
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/include/machine/float.h /usr/cross-freebsd/x86_64-pc-freebsd12/include/float.h
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/include/machine/floatingpoint.h /usr/cross-freebsd/x86_64-pc-freebsd12/include/floatingpoint.h
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/include/machine/stdarg.h /usr/cross-freebsd/x86_64-pc-freebsd12/include/stdarg.h
+
+ln -sf /usr/cross-freebsd/x86_64-pc-freebsd12/include/curses.h /usr/cross-freebsd/x86_64-pc-freebsd12/ncurses.h
