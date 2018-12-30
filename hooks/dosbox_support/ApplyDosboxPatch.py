4d6b810c5ca4e13d2cb861c738437e1d5104e4bd
Quentin PARIS <git@quentin.paris>
2018-12-30 12:35:38 +0100

    Dosbox support: bundle linux dependencies

--- hooks/dosbox_support/ApplyDosboxPatch.py
+++ hooks/dosbox_support/ApplyDosboxPatch.py
@@ -23,3 +23,16 @@ class ApplyDosboxPatch(AbstractHook):
         container.run(["cp", "/root/osxcross/target/macports/pkgs/opt/local/lib/libz.1.dylib", "/root/wine/lib/"])
         container.run(["cp", "/root/osxcross/target/macports/pkgs/opt/local/lib/libSDL_net-1.2.0.dylib", "/root/wine/lib/"])
         container.run(["cp", "/root/osxcross/target/macports/pkgs/opt/local/lib/libX11.6.dylib", "/root/wine/lib/"])
+
+    def _install_linux_dosbox(self, container):
+        container.run(["cp", "/usr/bin/dosbox", "/root/wine/bin/"])
+        container.run(["cp", "/usr/lib/i386-linux-gnu/libSDL-1.2.so.0", "/root/wine/lib/"])
+        container.run(["cp", "/usr/lib/i386-linux-gnu/libSDL_net-1.2.so.0", "/root/wine/lib/"])
+        container.run(["cp", "/usr/lib/i386-linux-gnu/libSDL_sound-1.0.so.1", "/root/wine/lib/"])
+        container.run(["cp", "/usr/lib/i386-linux-gnu/libcaca.so.0", "/root/wine/lib/"])
+        container.run(["cp", "/usr/lib/i386-linux-gnu/sse2/libspeex.so.1", "/root/wine/lib/"])
+        container.run(["cp", "/usr/lib/i386-linux-gnu/libvorbis.so.0", "/root/wine/lib/"])
+        container.run(["cp", "/usr/lib/i386-linux-gnu/libvorbisenc.so.2", "/root/wine/lib/"])
+        container.run(["cp", "/usr/lib/i386-linux-gnu/libvorbisfile.so.3", "/root/wine/lib/"])
+        container.run(["cp", "/usr/lib/i386-linux-gnu/libogg.so.0", "/root/wine/lib/"])
+        container.run(["cp", "/usr/lib/i386-linux-gnu/libFLAC.so", "/root/wine/lib/"])
