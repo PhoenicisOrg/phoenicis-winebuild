4a716855426b08200d6a2945c9fa5767074ebc35
ImperatorS79 <fevrier.simon@gmail.com>
2019-01-01 21:12:15 +0100

    Build seems to be working !

--- environments/freebsd/extractSDK.sh
+++ environments/freebsd/extractSDK.sh
@@ -6,12 +6,10 @@ tar -xf freebsd$VERSION.sdk.tar.xz
 cp -r freebsd$VERSION.sdk/usr/lib/*  /usr/cross-freebsd/x86_64-pc-freebsd$VERSION/lib
 cp -r freebsd$VERSION.sdk/usr/include/*  /usr/cross-freebsd/x86_64-pc-freebsd$VERSION/include
 cp -r freebsd$VERSION.sdk/lib/*  /usr/cross-freebsd/x86_64-pc-freebsd$VERSION/lib
-./symlink.sh
 
-mkdir -p /usr/cross-freebsd-32/i386-pc-freebsd$VERSION/include
-mkdir -p /usr/cross-freebsd-32/i386-pc-freebsd$VERSION/lib
+mkdir -p /usr/cross-freebsd/i386-pc-freebsd$VERSION/include
+mkdir -p /usr/cross-freebsd/i386-pc-freebsd$VERSION/lib
 
-tar -xf freebsd$VERSION.sdk.tar.xz
-cp -r freebsd$VERSION.sdk.32/usr/lib32/*  /usr/cross-freebsd-32/i386-pc-freebsd$VERSION/lib
-cp -r freebsd$VERSION.sdk/usr/include/*  /usr/cross-freebsd-32/i386-pc-freebsd$VERSION/include
-./symlink32.h
+tar -xf freebsd$VERSION.sdk.32.tar.xz
+cp -r freebsd$VERSION.sdk.32/usr/lib32/*  /usr/cross-freebsd/i386-pc-freebsd$VERSION/lib
+cp -r freebsd$VERSION.sdk/usr/include/*  /usr/cross-freebsd/i386-pc-freebsd$VERSION/include
