8abf7103f7f37a5da2035a8d1cebb4ceddb1098e
Quentin PARIS <git@quentin.paris>
2019-01-04 12:27:59 +0100

    Proton includes

---
+++ hooks/proton/AddProtonIncludes.py
@@ -0,0 +1,13 @@
+from hooks.AbstractHook import AbstractHook
+
+
+class AddProtonIncludes(AbstractHook):
+    def builder(self):
+        return "wine"
+
+    def event(self):
+        return "after-git"
+
+    def patch(self, container, operating_system, arch, version, distribution):
+        container.run(["git", "clone", "--progress", "https://github.com/ValveSoftware/Proton/", "/root/proton"])
+        container.run(["export", "C_INCLUDE_PATH=/root/proton/contrib/include"])
