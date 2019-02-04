1de311caa33c06c8aaf1fd3e25b5ee81fedd70b5
Gcenx <38226388+Gcenx@users.noreply.github.com>
2019-02-03 22:10:32 -0500

    Adding WineCX examples

---
+++ examples/example_linux_winecx_amd64.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+from packagers.PhoenicisWinePackageCreator import PhoenicisWinePackageCreator
+
+builder = PhoenicisWinePackageCreator()
+
+builder.build("cx", "winecx-18.1.0", "linux", "amd64")
