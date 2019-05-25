a4ddfd067edf9f468597708f3d9bb046f8d30741
Gcenx <gcenx83@gmail.com>
2019-05-25 17:18:43 -0400

    Added mac32 builder & example
    Instead of using mac64 to build 32bit, added mac32 along with an example to use it

---
+++ examples/example_osx_mac32.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+from packagers.PhoenicisWinePackageCreator import PhoenicisWinePackageCreator
+
+builder = PhoenicisWinePackageCreator()
+
+builder.build("upstream", "wine-4.0.1", "darwin", "mac32")
