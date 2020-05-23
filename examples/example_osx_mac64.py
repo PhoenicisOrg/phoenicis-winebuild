b7d9a992f30c60b56fe0244cf35f5246453a66fd
Gcenx <gcenx83@gmail.com>
2020-05-23 18:25:01 -0400

    Darwin Simplification
    Overhauled everything;
    - Use CodeWeavers Custom LLVM/Clang-8 for everything
    - No longer need to specify Includes/library files
    - Use OSXCROSS o32/o64-clang wrappers
    - Use OSXCROSS cmake wrappers
    - Added o32/o64-clang > gcc wrappers
    - Updated Ncurses/SDL2/MoltenVK/FAudio
    - Disabled VKD3D (currently usless on macOS)
    - Dropped 10.8 support
    - Added workaround for bug 49199
    - Dropped wine-tools -m32 wrapper

--- examples/example_osx_mac64.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-from packagers.PhoenicisWinePackageCreator import PhoenicisWinePackageCreator
-
-builder = PhoenicisWinePackageCreator()
-
-builder.build("upstream", "wine-4.0.1", "darwin", "mac64")
