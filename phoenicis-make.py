88ebb1b796b00ab456a6f1563c640c0458ead9b4
Quentin PARIS <git@quentin.paris>
2020-07-25 13:42:26 +0200

    Local build support

--- phoenicis-make.py
+++ phoenicis-make.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import sys
 
+from builders.WineBuilder import WineBuilder
 from core.Container import Container
 from core.Environment import Environment
 
@@ -27,28 +28,45 @@ parser.add_argument('--apply-patches', default=None,
 
 parser.add_argument('--environment', help='Environment to use')
 
+parser.add_argument('--os', help='Target OS to build')
+
+parser.add_argument('--architecture', help='Target architecture to build')
+
+parser.add_argument('--build', help="Build", action="store_true")
 args = parser.parse_args()
 
 if args.environment is None:
     fatal("You need to provide a valid environment (ex: --environment=wine_osxcross/linux/amd64)")
 
 wine_source = args.wine_src if args.wine_src is not None else os.getcwd()
+patches = [{
+    "name": patch,
+    "operatingSystems": args.os,
+    "architecture": args.architecture
+}  for patch in args.apply_patches] if args.apply_patches is not None else None
 
 try:
     environment = args.environment.split("/")
     build_environment = Environment(environment[0], environment[1], environment[2])
     info("Building environment...")
     build_environment.build()
-    container = Container(environment)
+    container = Container(build_environment)
     container.with_mount({
         "src": wine_source,
-        "dest": "/home/wine-git",
+        "dest": "/root/wine-git",
         "mode": "rw"
     })
     container.start()
+
+    builder = WineBuilder(container, patches)
+
+    if patches is not None:
+        builder.prepare(args.os, args.architecture, None, None, None, clone=True)
+
+    if args.build:
+        builder.do_build(args.os, args.architecture)
+
+    container.clean()
 except IndexError:
     fatal("You need to provide a valid environment (ex: --environment=wine_osxcross/linux/amd64)")
 
-
-print(args.apply_patches)
-print(wine_source)
