e7c3ce84053ff8a3560c22c3f9cb5512e7828ada
Quentin Pâris <github@playonlinux.com>
2021-08-08 19:51:56 +0200

    Clang refacto (#138)

--- environments/build-scripts/darwin/common/common.sh
+++ environments/build-scripts/darwin/common/common.sh
@@ -26,8 +26,8 @@ prepare() {
   export LIB_DIRECTORY
   export ABI
 
-  export CC="$ARCH-apple-darwin17-clang -L/opt/local/$LIB_DIRECTORY -stdlib=libc++ -I/opt/local/include"
-  export CXX="$ARCH-apple-darwin17-clang++ -L/opt/local/$LIB_DIRECTORY -stdlib=libc++ -I/opt/local/include"
+  #export CC="$ARCH-apple-darwin17-clang -L/opt/local/$LIB_DIRECTORY -stdlib=libc++ -I/opt/local/include"
+  #export CXX="$ARCH-apple-darwin17-clang++ -L/opt/local/$LIB_DIRECTORY -stdlib=libc++ -I/opt/local/include"
 
   MAJOR_VERSION="$(basename "$VERSION" | cut -d"." -f1,2)"
   export MAJOR_VERSION
@@ -35,21 +35,41 @@ prepare() {
 }
 
 install-libs() {
-  cd "/build/$ARCH/" || exit 3
+  cd "/build/$ARCH/opt/local/" || exit 3
   cp -r lib/* "/opt/local/$LIB_DIRECTORY" || exit 4
   cp -r include/* "/opt/local/include/" || exit 4
+  install-universal-libs
 }
 
 stage-libs() {
   mkdir -p "/staging/$LIB_DIRECTORY"
-  cd "/build/$ARCH/" || exit 3
+  cd "/build/$ARCH/opt/local/" || exit 3
   cp -r lib/* "/staging/$LIB_DIRECTORY" || exit 4
   cp -r include/* "/staging/include/" || exit 4
 }
 
 install-staging-libs() {
-  mv /staging/lib32/*  "/opt/local/lib32"
-  mv /staging/lib64/*  "/opt/local/lib64"
-  mv /staging/include/*  "/opt/local/include"
+  mv /staging/lib32/* "/opt/local/lib32"
+  mv /staging/lib64/* "/opt/local/lib64"
+  mv /staging/include/* "/opt/local/include"
+  install-universal-libs
   return 0
+}
+
+install-universal-libs() {
+  mkdir -p "/opt/local/lib/*.dylib"
+  for file32 in /opt/local/lib32/*.dylib; do
+    filename=$(basename "$file32")
+
+    [ "$filename" == "*.dylib" ] && break
+
+    if [ -e "/opt/local/lib64/$filename" ]; then
+      if [ ! -e "/opt/local/lib/$filename" ]; then
+        echo "Creating universal lib from $filename"
+        x86_64-apple-darwin17-lipo "/opt/local/lib32/$filename" "/opt/local/lib64/$filename" -output "/opt/local/lib/$filename" -create
+      else
+        echo "Universal lib $filename already exists. Skipping."
+      fi
+    fi
+  done
 }
--- environments/build-scripts/darwin/libtasn1.sh
+++ environments/build-scripts/darwin/libtasn1.sh
@@ -1,34 +1,16 @@
 #!/bin/bash
 
-if [ "$VERSION" = "" ]; then
-  echo "Please specify gnutls version (env VERSION)"
-  exit 1
-fi
+. /build-scripts/common.sh
 
-
-if [ "$ARCH" = "" ]; then
-  echo "Please specify architecture (env ARCH)"
-  exit 1
-fi
-
-[ "$ARCH" = "i386" ] && LIB_DIRECTORY="lib32"
-[ "$ARCH" = "x86_64" ] && LIB_DIRECTORY="lib64"
-
-rm -r "/build/"
-export CC="$ARCH-apple-darwin17-clang"
-export CXX="$ARCH-apple-darwin17-clang"
+prepare
 
 wget http://ftp.gnu.org/gnu/libtasn1/libtasn1-$VERSION.tar.gz || exit 1
 tar -xvf "libtasn1-$VERSION.tar.gz" || exit 1
 cd "libtasn1-$VERSION" || exit 1
 patch -p1 < /build-scripts/libtasn1.patch || exit 1
 
-./configure --host "$ARCH"-apple-darwin17 --prefix="/build/$ARCH/" --disable-valgrind-tests || exit 2
-make clean -j 4 || exit 2
+./configure --host "$ARCH"-apple-darwin17 --prefix="/opt/local" || exit 2
 make all -j 4 || exit 2
-make install -j 4 || exit 2
+make install DESTDIR="/build/$ARCH/" || exit 2
 
-
-cd "/build/$ARCH/" || exit 3
-cp -r lib/* "/opt/local/$LIB_DIRECTORY" || exit 4
-cp -r include/* "/opt/local/include/" || exit 4
+install-libs
