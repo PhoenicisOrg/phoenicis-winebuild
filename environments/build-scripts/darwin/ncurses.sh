e7b196cbad4798ae335b875c2366b12d7062bd52
Quentin PARIS <git@quentin.paris>
2023-02-11 19:30:31 +0100

    Fixed ncurses build

--- environments/build-scripts/darwin/ncurses.sh
+++ environments/build-scripts/darwin/ncurses.sh
@@ -10,6 +10,7 @@ cd "ncurses-$VERSION" || exit 1
 patch -p1 < /build-scripts/ncurses.patch || exit 1
 
 export STRIP="$ARCH-apple-darwin17-strip"
+export PKG_CONFIG_LIBDIR=/usr/lib/pkgconfig
 ./configure --host $ARCH-apple-darwin17 --with-shared --with-cxx-shared --enable-widec --disable-lib-suffixes --enable-overwrite --without-debug --without-ada  --with-manpage-format=normal --enable-pc-files --disable-mixed-case  --prefix="/opt/local" --enable-rpath --datarootdir=/usr/share || exit 2
 make -j 4 || exit 2
 make install DESTDIR="/build/$ARCH/"  || exit 2
